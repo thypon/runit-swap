c7e5e39d43e4d062e5f8c33e573624c36ce7af7c
Timofey Titovets <nefelim4ag@gmail.com>
2016-04-26 16:27:25 +0300

    Add Zswap configuration support

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd `dirname $0`
-sudo cp -v ./runit-swap.sh      /usr/sbin/runit-swap.sh
-sudo cp -v ./runit-swap.conf    /etc/
-sudo cp -rv ./swap              /etc/sv/swap
-sudo cp -v ./90-runit-swap.conf /etc/modprobe.d/90-runit-swap.conf
+[ "$UID" != "0" ] && [ ! -f /usr/bin/sudo ] && export SUDO="sudo"
+$SUDO cp -v ./runit-swap.sh      /usr/sbin/runit-swap.sh
+$SUDO cp -v ./runit-swap.conf    /etc/
+$SUDO cp -rv ./swap              /etc/sv/swap
+$SUDO cp -v ./90-runit-swap.conf /etc/modprobe.d/90-runit-swap.conf
