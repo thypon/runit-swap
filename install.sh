26789ec4402e0cfeea80fe8b4a9fd93d27695253
Timofey Titovets <nefelim4ag@gmail.com>
2016-05-11 03:45:26 +0300

    Use zram module hot add support instead of modprobe

--- install.sh
+++ install.sh
@@ -4,4 +4,3 @@ cd `dirname $0`
 $SUDO cp -v ./runit-swap.sh      /usr/sbin/runit-swap.sh
 $SUDO cp -v ./runit-swap.conf    /etc/
 $SUDO cp -rv ./swap              /etc/sv/swap
-$SUDO cp -v ./90-runit-swap.conf /etc/modprobe.d/90-runit-swap.conf
